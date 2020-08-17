5e67eb88bee71be1f9d4c324aeef8e4b4a9f2c69
Arup Dutta Bappy <ad.bappy10@gmail.com>
2020-08-17 20:09:14 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -30,10 +30,3 @@ function currentSlide(n){
 	Carousel(slideno=n);
 }
 
-
-function onClickMenu(){
-	document.getElementById("menu").classList.toggle("change");
-	document.getElementById("nav").classList.toggle("change");
-	
-	document.getElementById("menu-bg").classList.toggle("change-bg");
-}
